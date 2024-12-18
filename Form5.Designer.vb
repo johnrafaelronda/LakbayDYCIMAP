f442b72336edead917dc433f5953422a2a74262e
kyfrias <kylabiancafrias@gmail.com>
2024-12-19 00:51:58 +0800

    Add files via upload

--- Form5.Designer.vb
+++ Form5.Designer.vb
@@ -22,9 +22,93 @@ Partial Class Form5
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnAdmin = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.cbRoom = New System.Windows.Forms.ComboBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.flpMap = New System.Windows.Forms.FlowLayoutPanel()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(534, 73)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 46)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Room :"
+        '
+        'btnAdmin
+        '
+        Me.btnAdmin.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdmin.Location = New System.Drawing.Point(1629, 56)
+        Me.btnAdmin.Name = "btnAdmin"
+        Me.btnAdmin.Size = New System.Drawing.Size(162, 62)
+        Me.btnAdmin.TabIndex = 5
+        Me.btnAdmin.Text = "ADMIN"
+        Me.btnAdmin.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBack.Location = New System.Drawing.Point(45, 971)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(125, 47)
+        Me.btnBack.TabIndex = 6
+        Me.btnBack.Text = "BACK"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'cbRoom
+        '
+        Me.cbRoom.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbRoom.FormattingEnabled = True
+        Me.cbRoom.Location = New System.Drawing.Point(740, 73)
+        Me.cbRoom.Name = "cbRoom"
+        Me.cbRoom.Size = New System.Drawing.Size(396, 48)
+        Me.cbRoom.TabIndex = 7
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(740, 73)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(367, 48)
+        Me.txtSearch.TabIndex = 8
+        '
+        'flpMap
+        '
+        Me.flpMap.BackColor = System.Drawing.Color.Transparent
+        Me.flpMap.Location = New System.Drawing.Point(101, 195)
+        Me.flpMap.Name = "flpMap"
+        Me.flpMap.Size = New System.Drawing.Size(1715, 727)
+        Me.flpMap.TabIndex = 9
+        '
+        'Form5
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackgroundImage = Global.LakbayDYCI.My.Resources.Resources._462650719_537420329196995_90886213491450350_n__1_
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1924, 1050)
+        Me.Controls.Add(Me.flpMap)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.cbRoom)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.btnAdmin)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form5"
         Me.Text = "Form5"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnAdmin As Button
+    Friend WithEvents btnBack As Button
+    Friend WithEvents cbRoom As ComboBox
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents flpMap As FlowLayoutPanel
 End Class
--- Form5.vb
+++ Form5.vb
@@ -1,3 +1,149 @@
-﻿Public Class Form5
+﻿Imports System.IO
+Imports System.Drawing
 
+Public Class Form5
+
+    Dim roomNames As New List(Of String)
+    Dim roomImagePath As String = "C:\Users\user\lakbay map"
+
+    Private Sub RoomSearchApp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadRoomNames()
+        SetupAutocomplete()
+    End Sub
+
+    Private Sub LoadRoomNames()
+        roomNames.AddRange({"Finance Office", "Supreme Student Council", "Office of Student Affairs", "NTSP Paraya Office",
+                           "Purchasing Office", "Graduate School Office", "General Service Office", "Room 101", "Registrar Office",
+                           "Accounting Office", "Management System Office", "Human Resource Office", "Room 102", "Room 103", "Room 104",
+                           "Room 105", "Room 201", "Room 202", "Room 203", "Room 204", "Room 205", "College of Accountancy", "College of Arts",
+                           "College of Education", "College of Psychology", "College of Business Administration", "College of Computer Studies",
+                           "Audio Visual Room", "College of Health Sciences", "Guidance Office", "Health and Wellness Clinic", "School Canteen",
+                           "Center for Research and Development Office", "St.Carlo Acutis Chapel", "Huddle Room", "Scientia Hall 1", "Scientia Hall 2",
+                           "Science Laboratories", "Psychology Laboratory", "Arete Hall", "College Library", "Office of VP for Academic Affairs",
+                           "Lecture Room", "Sandbox", "EdTech", "Nexus", "Inspire Robotics", "Server Room", "Lecture Room AM 103",
+                           "Lecture Room AM 102", "Lecture Room AM 101", "Lecture Room AM 105", "Lecture Room AM 106", "Simulation Room", "Credo", "Utility Room", "Discussion Room",
+                           "Anatomy", "Physics", "Microbiology", "Chemistry", "Ampheather", "Skills Lab", "Student Park"})
+
+        If cbRoom IsNot Nothing Then
+            cbRoom.Items.AddRange(roomNames.ToArray())
+        End If
+    End Sub
+
+    Private Sub SetupAutocomplete()
+        If txtSearch IsNot Nothing Then
+            Dim autoComplete As New AutoCompleteStringCollection()
+            autoComplete.AddRange(roomNames.ToArray())
+
+            txtSearch.AutoCompleteMode = AutoCompleteMode.SuggestAppend
+            txtSearch.AutoCompleteSource = AutoCompleteSource.CustomSource
+            txtSearch.AutoCompleteCustomSource = autoComplete
+        End If
+    End Sub
+
+    Private Sub txtSearch_KeyDown(sender As Object, e As KeyEventArgs) Handles txtSearch.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            SearchRoom()
+        End If
+    End Sub
+
+    Private Sub cbRooms_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbRoom.SelectedIndexChanged
+        If cbRoom.SelectedItem IsNot Nothing Then
+            Dim selectedRoom As String = cbRoom.SelectedItem.ToString()
+            DisplayRoomImage(selectedRoom)
+        End If
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles txtSearch.Click
+        SearchRoom()
+    End Sub
+
+    Private Sub SearchRoom()
+        Dim roomName As String = txtSearch.Text.Trim()
+        If String.IsNullOrWhiteSpace(roomName) Then
+            MessageBox.Show("Please enter a room name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        If roomNames.Contains(roomName) Then
+            DisplayRoomImage(roomName)
+        Else
+            MessageBox.Show("Room not found. Please check the name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub DisplayRoomImage(roomName As String)
+        Try
+            ' Validate that the directory exists
+            If Not Directory.Exists(roomImagePath) Then
+                MessageBox.Show("The image directory does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            ' Search for image files named RoomName_1.png, RoomName_2.png, etc.
+            Dim imageFiles As String() = Directory.GetFiles(roomImagePath, roomName & "_*.png")
+
+            ' Clear any existing images in the FlowLayoutPanel
+            flpMap.Controls.Clear()
+
+            If imageFiles.Length > 0 Then
+                For Each filePath In imageFiles
+                    ' Load the image
+                    Dim img As Image = Image.FromFile(filePath)
+
+                    ' Calculate dimensions to fit the FlowLayoutPanel while maintaining aspect ratio
+                    Dim aspectRatio As Double = img.Width / img.Height
+                    Dim pbWidth As Integer = flpMap.ClientSize.Width - 20 ' Leave some padding
+                    Dim pbHeight As Integer = CInt(pbWidth / aspectRatio)
+
+                    ' Create a PictureBox with the calculated dimensions
+                    Dim pb As New PictureBox With {
+                        .Image = img,
+                        .SizeMode = PictureBoxSizeMode.StretchImage,
+                        .Width = pbWidth,
+                        .Height = pbHeight
+                    }
+
+                    flpMap.Controls.Add(pb)
+                Next
+
+                ' Make the FlowLayoutPanel scrollable
+                flpMap.AutoScroll = True
+                flpMap.FlowDirection = FlowDirection.TopDown
+                flpMap.WrapContents = False
+            Else
+                MessageBox.Show("No images found for " & roomName, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error loading image: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub flpMap_Resize(sender As Object, e As EventArgs) Handles flpMap.Resize
+        ' Adjust PictureBox sizes dynamically when FlowLayoutPanel resizes
+        For Each ctrl As Control In flpMap.Controls
+            If TypeOf ctrl Is PictureBox Then
+                Dim pb As PictureBox = DirectCast(ctrl, PictureBox)
+                Dim img As Image = pb.Image
+                If img IsNot Nothing Then
+                    Dim aspectRatio As Double = img.Width / img.Height
+                    pb.Width = flpMap.ClientSize.Width - 20
+                    pb.Height = CInt(pb.Width / aspectRatio)
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub btnAdmin_Click(sender As Object, e As EventArgs) Handles btnAdmin.Click
+        Me.Hide()
+        Form3.Show()
+    End Sub
+
+    Private Sub btnBACK_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Me.Hide()
+        Form2.Show()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        ' Placeholder for additional functionality
+    End Sub
 End Class
